4547aff2e47e15b66afe7181ba9feb20c918e03a
Bono <kubonkwan99@gmail.com>
2024-04-03 20:41:16 +0800

    feat: setup base physics and canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
-import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+import { KeyboardControls, Stats } from "@react-three/drei";
+import { Suspense, useMemo } from "react";
+import { Physics } from "@react-three/rapier";
+import { Leva } from "leva";
+import CameraPositionLogger from "./helper/CameraPositionLogger";
+
+export const Controls = {
+  left: "left",
+  right: "right",
+};
+
+const DEBUG = true;
 
 function App() {
-  return <div>Hello</div>;
+  const keyMap = useMemo(
+    () => [
+      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
+      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
+    ],
+    []
+  );
+
+  return (
+    <>
+      <Leva hidden={!DEBUG} />
+      <KeyboardControls map={keyMap}>
+        <Canvas
+          shadows
+          camera={{
+            position: [0, 0, 20],
+            fov: 40,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          {DEBUG ? (
+            <>
+              <CameraPositionLogger event="mousedown" />
+              <Stats />
+            </>
+          ) : undefined}
+          <color attach="background" args={["#ececec"]} />
+          <Suspense>
+            <Physics debug>
+              <Experience />
+            </Physics>
+          </Suspense>
+        </Canvas>
+        {/* <UI /> */}
+      </KeyboardControls>
+    </>
+  );
 }
 
 export default App;
