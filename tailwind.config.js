192976fc1ad0e77469d1452591b8caca28160cd8
Bono <kubonkwan99@gmail.com>
2024-04-05 16:15:42 +0800

    build: add font and clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nextgames: ["NEXTGAMES", "sans-serif"],
+      },
+      fontSize: {
+        clamp: "clamp(3rem, 13vw, 8rem)",
+      },
+      lineHeight: {
+        clamp: "clamp(3rem, 13vw, 8rem)",
+      },
+    },
   },
   plugins: [],
 };
