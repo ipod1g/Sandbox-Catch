882bb335a904683fd294f6b248040e2ff715183a
Bono <kubonkwan99@gmail.com>
2024-04-07 23:55:53 +0800

    feat: setup axios and react query

---
+++ src/lib/axios.ts
@@ -0,0 +1,15 @@
+import Axios from "axios";
+import { API_URL } from "../config";
+
+export const instance = Axios.create({
+  baseURL: API_URL,
+});
+
+instance.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
